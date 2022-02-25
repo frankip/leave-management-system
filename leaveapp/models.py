05701d1df09cd07bb7f469a0e24d25da2e497614
Francky <gwadafrank@gmail.com>
2022-02-26 00:30:08 +0300

    update model and add migrations

--- leaveapp/models.py
+++ leaveapp/models.py
@@ -1,34 +1,33 @@
-import email
-from operator import mod
-from unicodedata import name
+from datetime import date
 from django.db import models
 
 # Create your models here.
 
 class Role(models.Model):
-    role_title = models.CharField(max_length=100)
+    role_title = models.CharField(max_length=100, verbose_name="Role")
     role_description = models.TextField()
 
     def __str__(self):
         return self.role_title
 
+
 class Employee(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, verbose_name="Full names")
     email = models.EmailField()
     date_of_birth = models.DateField()
     address = models.CharField(max_length=250)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
 
-    def _str__(self):
+    def __str__(self):
         return self.name
 
+ 
 class LeaveType(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Leave")
     description = models.TextField()
 
     def __str__(self):
         return self.name
-
 
 
 class Leave(models.Model):
@@ -38,17 +37,16 @@ class Leave(models.Model):
         ('PENDING', 'Pending')
     )
 
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    employee = models.ForeignKey("Employee", on_delete=models.CASCADE)
     type = models.ForeignKey(LeaveType, on_delete=models.CASCADE)
-    description = models.TextField()
-    status = models.CharField(max_length=50, choices=STATUS)
-    start_date = models.DateField()
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=50, choices=STATUS, default="PENDING")
+    start_date = models.DateField(default=date.today)
     end_date = models.DateField()
     applied_date = models.DateField(auto_now_add=True)
 
-
     def __str__(self):
-        return self.employee + self.type 
+        return f'{self.employee.name} - {self.type.name}'
 
 
 
